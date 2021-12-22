316374abef723aa9aa9185e49ea4b82676286d1f
Han Pham <hanpham@Hans-MacBook-Pro.local>
2021-12-21 17:37:18 -0800

    Minor landing page updates

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -8,25 +8,31 @@ function Hero() {
     >
       <div className="pt-24 pb-16 px-4 text-white flex flex-col items-center">
         <p className="lg:text-2xl text-xl">
-          Student-run football NFTs collection
+          An NFT Aggregation Platform for College Sports Communities
         </p>
         <p className="text-left lg:w-96 mt-4 lg:my-2">
-          Our NFT collection is guaranteed to be unique and their scarcity is
-          ensured using <strong>blockchain</strong> technology and the use of{" "}
-          <strong>smart contracts</strong>.
+          Starting from unique digital collectibles that live on the Ethereum
+          blockchain, we plan to build a vibrant ecosystem that provides
+          features like community token issuance, decentralized finance (DeFi),
+          and play-to-earn DeFi games. A decentralized autonomous
+          Organization(DAO) makes sure every voice in the community is heard.
         </p>
         <p className="text-left lg:w-96 mt-4 lg:my-2">
-          The collections are well-designed and hand-drawn by our creative UW
-          student designers.
+          We are a team of students at the University of Washington trying to
+          bring the best in blockchain technology to our community. This project
+          aims to help college athletes and artists build a fan base and
+          supercharge the community to be closer and more engaging than ever.
         </p>
         <p className="text-left lg:w-96 mt-4 lg:my-2">
-          Owning an NFT shows your support to UW student designers and sport
-          teams, as well as your support to the future of cryptocurrency and
-          Metaverse. We aim to build and facilitate an ecosystem and community
-          through our athletes NFTs stories.
+          Owning a CML NFT means you are a stakeholder of our project and an
+          investor in the future of blockchain technology, cryptocurrency, and
+          the Metaverse. It also shows your support to student designers and
+          college athletes.
         </p>
         <p className="mt-4">Inquiry:</p>
-        <a href = "mailto: CryptoMegaLeague@gmail.com" className="underline">CryptoMegaLeague@gmail.com</a>
+        <a href="mailto: CryptoMegaLeague@gmail.com" className="underline">
+          CryptoMegaLeague@gmail.com
+        </a>
       </div>
       {/* <NewsletterForm></NewsletterForm> */}
     </div>
